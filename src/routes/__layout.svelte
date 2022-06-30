d6f8a4a3567301586ec198dbbcc22f5995a2e1e2
ptums <ptumulty923@gmail.com>
2022-06-29 23:46:54 -0400

    finished header and mobile menu

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,7 @@
 <div class="min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
   <Header />
   <div class="max-w-xs py-12 mx-auto sm:max-w-screen-sm md:max-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
-    <div class="w-full bg-white shadow sm:rounded-md">
+    <div class="w-full bg-white shadow-lg rounded">
       <slot />
     </div>
  </div>
