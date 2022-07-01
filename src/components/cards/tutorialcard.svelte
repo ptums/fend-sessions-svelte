ae36e357f3dd9c3b0547ce089d521db85b4d3f6d
ptums <ptumulty923@gmail.com>
2022-06-30 23:02:07 -0400

    finished tutorial page

---
+++ src/components/cards/tutorialcard.svelte
@@ -0,0 +1,29 @@
+<script>
+   export let category;
+   export let title;
+   export let url;
+
+   import { generateLabelColors} from "../../utils/helper.ts";
+</script>
+<li class="block hover:bg-gray-50">
+      <div class="px-4 py-4 sm:px-6">
+       <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+          <p class="mb-2 text-base font-medium text-indigo-600 truncate sm:mb-0">{title}</p>
+          <div class="flex flex-shrink-0 sm:ml-2">
+            <p class={`inline-flex px-2 text-xs font-semibold ${generateLabelColors(category)} rounded-full leading-5`}>
+                 {category}
+             </p>
+           </div>
+        </div>
+        <div class="mt-2 sm:flex sm:justify-between">
+           <div class="sm:flex">
+              <p class="flex flex-row items-center h-5 text-sm text-gray-500">
+                  <a href={url} class="mr-2 underline" target="_blank" rel="noopener noreferrer nofollow">
+                    {url}  
+                  </a>
+              </p>                   
+           </div>
+           
+        </div>
+      </div>
+  </li>
--- src/routes/tutorials.svelte
+++ src/routes/tutorials.svelte
@@ -1,3 +1,56 @@
-<div>
-    Cards here..
-</div>
+<script context="module">
+   import { gql, GraphQLClient } from 'graphql-request'
+   
+   export async function load() {
+      const url = "https://api-us-east-1.graphcms.com/v2/cl43i3zwv5dty01xjaab6hf3j/master";
+ 
+    const graphcms = new GraphQLClient(
+      url, 
+      {
+        headers: {},
+      }
+    )
+
+    const query = gql`
+      query {
+         tutorials {
+            title
+            url
+            category
+         }
+      }
+    `;
+
+    const { tutorials } = await graphcms.request(query)
+
+    return {
+      props: {
+        tutorials,
+      },
+    }
+  }
+
+</script>
+
+<script>
+   export let tutorials;
+   console.log({ tutorials })
+   const sortedTutorials = tutorials.sort((a,b) => a?.category.trim().toLowerCase() > b?.category.trim().toLowerCase() ? 1 : -1);
+   import TutorialCard from '../components/cards/tutorialcard.svelte';
+</script>
+
+<head>
+  <title>
+</head>
+<ul role="list" class="divide-y divide-gray-200">
+  {#each sortedTutorials as tutorial}
+   <TutorialCard
+      title={tutorial?.title}
+      category={tutorial?.category}
+      url={tutorial?.url}
+   /> 
+  {/each}
+</ul>
+
+
+
